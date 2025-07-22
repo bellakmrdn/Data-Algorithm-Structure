c5b08b53e7d9fae9f4e287d5653e193d49c84534
bellakmrdn <hidayatulnabila873@gmail.com>
2025-07-22 22:12:03 +0800

    Add files via upload

---
+++ PROJECT/MergeSort Application/ui_form5.h
@@ -0,0 +1,364 @@
+/********************************************************************************
+** Form generated from reading UI file 'form5.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_FORM5_H
+#define UI_FORM5_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_form5
+{
+public:
+    QLabel *lbl3Bubble;
+    QLabel *lbl4Bubble;
+    QLabel *lbl2Bubble;
+    QLabel *lbl5Bubble;
+    QLabel *lbl1Bubble;
+    QLabel *lbl10Bubble;
+    QLabel *lbl7Bubble;
+    QLabel *lbl6Bubble;
+    QLabel *lbl8Bubble;
+    QLabel *lbl9Bubble;
+    QLabel *lbl15Bubble;
+    QLabel *lbl12Bubble;
+    QLabel *lbl11Bubble;
+    QLabel *lbl13Bubble;
+    QLabel *lbl14Bubble;
+    QLabel *lbl20Bubble;
+    QLabel *lbl17Bubble;
+    QLabel *lbl16Bubble;
+    QLabel *lbl18Bubble;
+    QLabel *lbl19Bubble;
+    QPushButton *btnPrev;
+    QPushButton *btnNext;
+
+    void setupUi(QWidget *form5)
+    {
+        if (form5->objectName().isEmpty())
+            form5->setObjectName("form5");
+        form5->resize(678, 495);
+        form5->setAutoFillBackground(false);
+        form5->setStyleSheet(QString::fromUtf8("\n"
+"background-color: rgb(251, 242, 255);\n"
+""));
+        lbl3Bubble = new QLabel(form5);
+        lbl3Bubble->setObjectName("lbl3Bubble");
+        lbl3Bubble->setGeometry(QRect(80, 170, 71, 61));
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Verdana")});
+        font.setPointSize(22);
+        lbl3Bubble->setFont(font);
+        lbl3Bubble->setAutoFillBackground(false);
+        lbl3Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl3Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl3Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl4Bubble = new QLabel(form5);
+        lbl4Bubble->setObjectName("lbl4Bubble");
+        lbl4Bubble->setGeometry(QRect(80, 230, 71, 61));
+        lbl4Bubble->setFont(font);
+        lbl4Bubble->setAutoFillBackground(false);
+        lbl4Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl4Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl4Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl2Bubble = new QLabel(form5);
+        lbl2Bubble->setObjectName("lbl2Bubble");
+        lbl2Bubble->setGeometry(QRect(80, 110, 71, 61));
+        lbl2Bubble->setFont(font);
+        lbl2Bubble->setAutoFillBackground(false);
+        lbl2Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl2Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl2Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl5Bubble = new QLabel(form5);
+        lbl5Bubble->setObjectName("lbl5Bubble");
+        lbl5Bubble->setGeometry(QRect(80, 290, 71, 61));
+        lbl5Bubble->setFont(font);
+        lbl5Bubble->setAutoFillBackground(false);
+        lbl5Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl5Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl5Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl1Bubble = new QLabel(form5);
+        lbl1Bubble->setObjectName("lbl1Bubble");
+        lbl1Bubble->setGeometry(QRect(80, 50, 71, 61));
+        QFont font1;
+        font1.setFamilies({QString::fromUtf8("Verdana")});
+        font1.setPointSize(22);
+        font1.setBold(false);
+        lbl1Bubble->setFont(font1);
+        lbl1Bubble->setAutoFillBackground(false);
+        lbl1Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl1Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl1Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl10Bubble = new QLabel(form5);
+        lbl10Bubble->setObjectName("lbl10Bubble");
+        lbl10Bubble->setGeometry(QRect(220, 290, 71, 61));
+        lbl10Bubble->setFont(font);
+        lbl10Bubble->setAutoFillBackground(false);
+        lbl10Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl10Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl10Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl7Bubble = new QLabel(form5);
+        lbl7Bubble->setObjectName("lbl7Bubble");
+        lbl7Bubble->setGeometry(QRect(220, 110, 71, 61));
+        lbl7Bubble->setFont(font);
+        lbl7Bubble->setAutoFillBackground(false);
+        lbl7Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl7Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl7Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl6Bubble = new QLabel(form5);
+        lbl6Bubble->setObjectName("lbl6Bubble");
+        lbl6Bubble->setGeometry(QRect(220, 50, 71, 61));
+        lbl6Bubble->setFont(font1);
+        lbl6Bubble->setAutoFillBackground(false);
+        lbl6Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl6Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl6Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl8Bubble = new QLabel(form5);
+        lbl8Bubble->setObjectName("lbl8Bubble");
+        lbl8Bubble->setGeometry(QRect(220, 170, 71, 61));
+        lbl8Bubble->setFont(font);
+        lbl8Bubble->setAutoFillBackground(false);
+        lbl8Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl8Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl8Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl9Bubble = new QLabel(form5);
+        lbl9Bubble->setObjectName("lbl9Bubble");
+        lbl9Bubble->setGeometry(QRect(220, 230, 71, 61));
+        lbl9Bubble->setFont(font);
+        lbl9Bubble->setAutoFillBackground(false);
+        lbl9Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl9Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl9Bubble->setScaledContents(false);
+        lbl9Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl15Bubble = new QLabel(form5);
+        lbl15Bubble->setObjectName("lbl15Bubble");
+        lbl15Bubble->setGeometry(QRect(370, 290, 71, 61));
+        lbl15Bubble->setFont(font);
+        lbl15Bubble->setAutoFillBackground(false);
+        lbl15Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl15Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl15Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl12Bubble = new QLabel(form5);
+        lbl12Bubble->setObjectName("lbl12Bubble");
+        lbl12Bubble->setGeometry(QRect(370, 110, 71, 61));
+        lbl12Bubble->setFont(font);
+        lbl12Bubble->setAutoFillBackground(false);
+        lbl12Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl12Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl12Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl11Bubble = new QLabel(form5);
+        lbl11Bubble->setObjectName("lbl11Bubble");
+        lbl11Bubble->setGeometry(QRect(370, 50, 71, 61));
+        lbl11Bubble->setFont(font1);
+        lbl11Bubble->setAutoFillBackground(false);
+        lbl11Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl11Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl11Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl13Bubble = new QLabel(form5);
+        lbl13Bubble->setObjectName("lbl13Bubble");
+        lbl13Bubble->setGeometry(QRect(370, 170, 71, 61));
+        lbl13Bubble->setFont(font);
+        lbl13Bubble->setAutoFillBackground(false);
+        lbl13Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl13Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl13Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl14Bubble = new QLabel(form5);
+        lbl14Bubble->setObjectName("lbl14Bubble");
+        lbl14Bubble->setGeometry(QRect(370, 230, 71, 61));
+        lbl14Bubble->setFont(font);
+        lbl14Bubble->setAutoFillBackground(false);
+        lbl14Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl14Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl14Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl20Bubble = new QLabel(form5);
+        lbl20Bubble->setObjectName("lbl20Bubble");
+        lbl20Bubble->setGeometry(QRect(520, 290, 71, 61));
+        lbl20Bubble->setFont(font);
+        lbl20Bubble->setAutoFillBackground(false);
+        lbl20Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl20Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl20Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl17Bubble = new QLabel(form5);
+        lbl17Bubble->setObjectName("lbl17Bubble");
+        lbl17Bubble->setGeometry(QRect(520, 110, 71, 61));
+        lbl17Bubble->setFont(font);
+        lbl17Bubble->setAutoFillBackground(false);
+        lbl17Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl17Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl17Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl16Bubble = new QLabel(form5);
+        lbl16Bubble->setObjectName("lbl16Bubble");
+        lbl16Bubble->setGeometry(QRect(520, 50, 71, 61));
+        lbl16Bubble->setFont(font1);
+        lbl16Bubble->setAutoFillBackground(false);
+        lbl16Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl16Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl16Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl18Bubble = new QLabel(form5);
+        lbl18Bubble->setObjectName("lbl18Bubble");
+        lbl18Bubble->setGeometry(QRect(520, 170, 71, 61));
+        lbl18Bubble->setFont(font);
+        lbl18Bubble->setAutoFillBackground(false);
+        lbl18Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl18Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl18Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        lbl19Bubble = new QLabel(form5);
+        lbl19Bubble->setObjectName("lbl19Bubble");
+        lbl19Bubble->setGeometry(QRect(520, 230, 71, 61));
+        lbl19Bubble->setFont(font);
+        lbl19Bubble->setAutoFillBackground(false);
+        lbl19Bubble->setStyleSheet(QString::fromUtf8("background-color: white;\n"
+"border: 1px solid black;\n"
+"color: black;\n"
+"padding: 3px;\n"
+""));
+        lbl19Bubble->setFrameShape(QFrame::Shape::Panel);
+        lbl19Bubble->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        btnPrev = new QPushButton(form5);
+        btnPrev->setObjectName("btnPrev");
+        btnPrev->setGeometry(QRect(220, 400, 101, 51));
+        QFont font2;
+        font2.setFamilies({QString::fromUtf8("Arial Rounded MT")});
+        font2.setPointSize(8);
+        font2.setBold(true);
+        btnPrev->setFont(font2);
+        btnPrev->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);\n"
+"background-color: rgb(204, 194, 207);\n"
+"gridline-color: rgb(0, 0, 0);\n"
+"border-color: rgb(0, 0, 0);"));
+        btnNext = new QPushButton(form5);
+        btnNext->setObjectName("btnNext");
+        btnNext->setGeometry(QRect(350, 400, 91, 51));
+        btnNext->setFont(font2);
+        btnNext->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);\n"
+"background-color: rgb(204, 194, 207);\n"
+"gridline-color: rgb(0, 0, 0);\n"
+"border-color: rgb(0, 0, 0);"));
+
+        retranslateUi(form5);
+
+        QMetaObject::connectSlotsByName(form5);
+    } // setupUi
+
+    void retranslateUi(QWidget *form5)
+    {
+        form5->setWindowTitle(QCoreApplication::translate("form5", "Form", nullptr));
+        lbl3Bubble->setText(QString());
+        lbl4Bubble->setText(QString());
+        lbl2Bubble->setText(QString());
+        lbl5Bubble->setText(QString());
+        lbl1Bubble->setText(QString());
+        lbl10Bubble->setText(QString());
+        lbl7Bubble->setText(QString());
+        lbl6Bubble->setText(QString());
+        lbl8Bubble->setText(QString());
+        lbl9Bubble->setText(QString());
+        lbl15Bubble->setText(QString());
+        lbl12Bubble->setText(QString());
+        lbl11Bubble->setText(QString());
+        lbl13Bubble->setText(QString());
+        lbl14Bubble->setText(QString());
+        lbl20Bubble->setText(QString());
+        lbl17Bubble->setText(QString());
+        lbl16Bubble->setText(QString());
+        lbl18Bubble->setText(QString());
+        lbl19Bubble->setText(QString());
+        btnPrev->setText(QCoreApplication::translate("form5", "Previous Pass", nullptr));
+        btnNext->setText(QCoreApplication::translate("form5", "Next Pass", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class form5: public Ui_form5 {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_FORM5_H
